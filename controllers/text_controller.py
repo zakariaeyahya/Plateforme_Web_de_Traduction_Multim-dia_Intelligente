1a5de0830d52faab1ec546c983205d290cb67ec1
YAHYA <155691167+zakariaeyahya@users.noreply.github.com>
2024-08-17 23:06:14 +0200

    Update text_controller.py

--- controllers/text_controller.py
+++ controllers/text_controller.py
@@ -7,6 +7,7 @@ class TextController:
     def translate_text(self, text, src_lang, tgt_lang):
         try:
             translated_text = self.translator.translate_text(text, src_lang, tgt_lang)
+            translated_text=clean_output(translated_text)
             return {"status": "success", "translated_text": translated_text}
         except Exception as e:
             return {"status": "error", "message": str(e)}
