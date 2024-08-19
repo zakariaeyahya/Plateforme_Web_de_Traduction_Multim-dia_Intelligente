9b4a467ee8c2467f32d66eacbb649edb6f8f958b
YAHYA <155691167+zakariaeyahya@users.noreply.github.com>
2024-08-19 13:43:49 +0200

    Update clean_output.py

--- services/service_functions/clean_output.py
+++ services/service_functions/clean_output.py
@@ -1,6 +1,6 @@
 import re
 
 def clean_output(text):
-    cleaned_text = text.replace('</s>', '').strip()
+    cleaned_text = text.replace("<unk>", "").replace("</s>", "").strip()
     cleaned_text = re.sub(r'__\w+__\s*', '', cleaned_text)
     return cleaned_text
